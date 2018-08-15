f9447c75efc6569bfd0361015999a4a08b5b406d
Krypt <irwenyap2001@gmail.com>
2018-08-15 21:58:59 +0800

    Revert "Merge branch 'master' of https://github.com/Krypt-GitHub/Studio_Project_One"
    This reverts commit 6b21d9de9bb75b79af71182831d23c5f94c0982e, reversing
    changes made to 0f2b0aa826cffd503434cf8647d1ccb3bcc53a90.

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -27,8 +27,7 @@ enum EGAMESTATES
 {
     S_SPLASHSCREEN,
     S_GAME,
-    S_COUNT,
-	S_GAMEOVER
+    S_COUNT
 };
 
 // struct for the game character
@@ -57,8 +56,5 @@ void renderCharacter();     // renders the character into the buffer
 void characterInteraction();
 void renderFramerate();     // renders debug information, frame rate, elapsed time, etc
 void renderToScreen();      // dump the contents of the buffer to the screen, one frame worth of game
-void Level1AIMove();
-void renderGameOver();//For AI to move forward
-void gameoverwait();
-void gameovercondition();//For AI to move back
+void Level1AIMovement();
 #endif // _GAME_H
